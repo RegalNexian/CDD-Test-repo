9bc5335e02aef3e5fbbcc1a03d199a2b57d38ee6
Ayushmancodes-08 <patraayushman21@gmail.com>
2025-02-15 17:54:50 +0530

    CDD-test commit

---
+++ Crypter/script.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    document.body.style.background = "red";
+}
+
+function ctn() {
+    document.body.style.background = "blue";
+}
+
+
+function gtn() {
+    document.body.style.background = "green";
+}
+
