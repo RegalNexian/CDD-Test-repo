72e34cd2617ed457b210096b508e5914ff03f041
TRUPTI1203 <truptimayeesuar@gmail.com>
2025-02-16 20:59:32 +0530

    trupti created

---
+++ tscript.js
@@ -0,0 +1,9 @@
+function Red() {
+  document.getElementById("page").style.background = "#e33353";
+}
+function Blue() {
+  document.getElementById("page").style.background = "#46a2da";
+}
+function Green() {
+  document.getElementById("page").style.background = "#39e75f";
+}
